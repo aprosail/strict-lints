86d4ddb9621c09646008b9ed21ba7cbfa919af6b
James Aprosail <aprosail@outlook.com>
2024-05-10 17:49:40 +0800

    example spider show summarize

--- example/spider_example.dart
+++ example/spider_example.dart
@@ -4,7 +4,9 @@ import 'package:terminal_decorate/terminal_decorate.dart';
 void main() async {
   print('getting from: ${site.cyan}'.blue);
   print('...'.dim);
+
   final rules = await spider();
+  var count = 0;
   for (final name in rules.keys) {
     final status = rules[name]!;
     print([
@@ -12,10 +14,17 @@ void main() async {
         if (status.hasReuse) return name.dim;
         if (status.removed) return name.red.crossLine;
         if (status.unreleased) return name.yellow.crossLine;
+        count++;
         if (status.hasFix) return name.green;
         return name;
       }(),
       if (status.isNotEmpty) '(${status.format})'.dim,
     ].join(' '));
   }
+  print(
+    '\n'
+    'all ${rules.keys.length.toString().blue} rules, '
+    '${count.toString().green} configurable rules.'
+    '\n',
+  );
 }
